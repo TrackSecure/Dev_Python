b0d6dd344e68b3fe6f33b473626e54bfe3e2d774
bruno-yuji <bruno.ytakahashi@sptech.school>
2024-10-11 19:38:09 -0300

    Captura de processos atualizada para remover unidade "K" do uso de memória

--- captura_processos.py
+++ captura_processos.py
@@ -8,12 +8,14 @@ result = subprocess.run(['tasklist'], capture_output=True, text=True).stdout
 
 print(result)
 
-result = re.sub(r" (?![0-9 ]|(Services)|(Console))", "_", result)
+result = re.sub(r" K(?=\n)", "", result) # Expressão Regular para remover os "K" no uso de memória
+result = re.sub(r" (?![0-9 ]|(Services)|(Console))", "_", result) # Expressão Regular para substituir espaços de palavras por '_'
+
 lines = result.splitlines()[3:]
 
 with open('tasks3.csv', 'w', newline='', encoding='utf-8') as csvfile:
     csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(['Nome_da_Imagem', 'PID', 'Nome_da_Sessao', 'Sessao#', 'Uso_de_memoria'])
+    csv_writer.writerow(['Nome_da_Imagem', 'PID', 'Nome_da_Sessao', 'Sessao#', 'Uso_de_memoria (K)'])
    
     for line in lines:
         columns = line.split()
