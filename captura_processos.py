0bd9d49c6f75b8348a5a07874067003525fa22d7
bruno-yuji <bruno.ytakahashi@sptech.school>
2024-10-11 18:39:10 -0300

    Script de captura de processos arrumado

--- captura_processos.py
+++ captura_processos.py
@@ -1,29 +1,20 @@
 import subprocess
 import csv
+import re
 
 
 # Executar o comando 'tasklist' e capturar a saída - este comando é o mesmo do CMD - sistema operacional
-result = subprocess.run(['tasklist'], capture_output=True, text=True)
+result = subprocess.run(['tasklist'], capture_output=True, text=True).stdout
 
-# Verificar o resultado da saída
 print(result)
 
-# Separar as linhas da saída está dando erro de ecoding no comando original
-#lines = result.stdout.splitlines()
-lines = result.stdout.splitlines()[2:]
+result = re.sub(r" (?![0-9 ]|(Services)|(Console))", "_", result)
+lines = result.splitlines()[3:]
 
-# Abrir um arquivo CSV para escrita com codificação UTF-8  na mesma pasta deste projeto 
 with open('tasks3.csv', 'w', newline='', encoding='utf-8') as csvfile:
-    
-    # Criar um objeto escritor CSV para importar
     csv_writer = csv.writer(csvfile)
-
-    # Escrever o cabeçalho (opcional, ajustável de acordo com a saída do 'tasklist')
     csv_writer.writerow(['Nome_da_Imagem', 'PID', 'Nome_da_Sessao', 'Sessao#', 'Uso_de_memoria'])
    
-
-    # Escrever as linhas de dados
     for line in lines:
-        # Dividir a linha em colunas (ajustar os delimitadores se necessário)
         columns = line.split()
         csv_writer.writerow(columns)
